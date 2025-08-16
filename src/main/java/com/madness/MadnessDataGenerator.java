a6aaa8e7574ef4c745d112d2d15c0035367014a0
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-08-15 21:42:49 -0300

    Init

---
+++ src/main/java/com/madness/MadnessDataGenerator.java
@@ -0,0 +1,11 @@
+package com.madness;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MadnessDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
